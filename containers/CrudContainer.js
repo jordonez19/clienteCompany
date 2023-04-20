8ed8028beba6b2a1c6d0f13a60697ad8ef052525
xavi <javier19pedraza@gmail.com>
2023-04-19 20:40:17 -0500

    content

--- containers/CrudContainer.js
+++ containers/CrudContainer.js
@@ -1,8 +1,48 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
+import { Table, Col, Row, Button, Modal, Input } from "antd";
+
+const columns = [
+  {
+    title: "#",
+    dataIndex: "id",
+    key: "id",
+  },
+  {
+    title: "Nombre",
+    dataIndex: "name",
+    key: "name",
+  },
+  {
+    title: "Precio",
+    dataIndex: "price",
+    key: "price",
+  },
+  {
+    title: "Descripcion",
+    dataIndex: "description",
+    key: "description",
+  },
+];
+
 const CrudContainer = () => {
   const [data, setData] = useState();
+
+  
+  const [openModal, setOpenModal] = useState(true);
+
+  const [mode, setMode] = useState("");
+
+  const showModal = () => {
+    setOpenModal(true);
+  };
+  const handleOk = () => {
+    setOpenModal(false);
+  };
+  const handleCancel = () => {
+    setOpenModal(false);
+  };
 
   useEffect(() => {
     fetchdata();
@@ -12,36 +52,45 @@ const CrudContainer = () => {
     const response = await axios.get("http://localhost:2000/products/");
     setData(response.data);
   };
+
   return (
-    <>
-      <h1>Mi primera api</h1>
-      <table border="solid">
-        <thead>
-          <tr>
-            <td>id</td>
-            <td>name</td>
-            <td>price</td>
-            <td>description</td>
-          </tr>
-        </thead>
-        <tbody>
-          {data ? (
-            data.map((item) => (
-              <tr key={item.id}>
-                <td>{item.id}</td>
-                <td>{item.name}</td>
-                <td>{item.price}</td>
-                <td>{item.description}</td>
-              </tr>
-            ))
-          ) : (
-            <tr>
-              <td colSpan="4">No hay data</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
-    </>
+    <div>
+      <Row>
+        <Col span={12} offset={6}>
+          <h1>Mi primera api</h1>
+          <Button type="primary" onClick={showModal}>
+            Crear
+          </Button>
+          <Table dataSource={data} columns={columns} />
+        </Col>
+      </Row>
+
+      <Button onClick={() => {
+
+        setMode("update")
+        setOpenModal(true)
+
+        }}>update</Button>
+
+      <Button onClick={() => {
+        setMode("CREAR")
+        setOpenModal(true)
+        }}>CREAR</Button>
+
+      <Modal
+        title={mode === "update" ? "Actualizar producto" : "crear producto"}
+        open={openModal}
+        onOk={() => setOpenModal(false)}
+        onCancel={() => setOpenModal(false)}
+      >
+        <p>Some contents...</p>
+        <Input placeholder="content" />
+        <p>Some contents...</p>
+        <Input placeholder="content" />
+        <p>Some contents...</p>
+        <Input placeholder="content" />
+      </Modal>
+    </div>
   );
 };
 
